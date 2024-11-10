332445e9b68bcdc2bd88a6724286bbd680623b2e
Hakan Shehu <hakanshehu15@gmail.com>
2024-11-10 10:14:24 +0100

    Temporarily disable email init

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,7 +3,7 @@ import { initRedis } from '@/data/redis';
 import { migrate } from '@/data/database';
 import { initEventWorker } from '@/queues/events';
 import { initTaskWorker } from '@/queues/tasks';
-import { initEmail } from '@/services/email';
+// import { initEmail } from '@/services/email';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,7 +11,7 @@ dotenv.config();
 const init = async () => {
   await migrate();
   await initRedis();
-  await initEmail();
+  // await initEmail();
   await initApi();
 
   initEventWorker();

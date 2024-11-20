8d4a57e866e98dde449e1b36e70d030e30dd4d44
Hakan Shehu <hakanshehu15@gmail.com>
2024-11-20 12:27:37 +0100

    Fix UI issue in emoji and icon picker

--- apps/desktop/src/renderer/components/icons/icon-picker-search.tsx
+++ apps/desktop/src/renderer/components/icons/icon-picker-search.tsx
@@ -15,7 +15,7 @@ export const IconPickerSearch = ({ query }: IconPickerSearchProps) => {
   }, [query]);
 
   return (
-    <div className="grid h-full min-h-full w-full min-w-full grid-cols-10 gap-1">
+    <div className="grid w-full min-w-full grid-cols-10 gap-1">
       <div className="col-span-full flex items-center py-1 pl-1 text-sm text-muted-foreground">
         <p>Search results for "{query}"</p>
       </div>

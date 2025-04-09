90f8cf95f1a741c978abdad5cec9bd4e0bba2b11
Hakan Shehu <hakanshehu15@gmail.com>
2025-04-09 20:07:43 +0200

    Implement infinite loading for table view

--- apps/desktop/src/renderer/components/databases/tables/table-view-body.tsx
+++ apps/desktop/src/renderer/components/databases/tables/table-view-body.tsx
@@ -1,5 +1,6 @@
+import { InView } from 'react-intersection-observer';
+
 import { TableViewEmptyPlaceholder } from '@/renderer/components/databases/tables/table-view-empty-placeholder';
-import { TableViewLoadMoreRow } from '@/renderer/components/databases/tables/table-view-load-more-row';
 import { TableViewRow } from '@/renderer/components/databases/tables/table-view-row';
 import { useDatabaseView } from '@/renderer/contexts/database-view';
 import { useRecordsQuery } from '@/renderer/hooks/use-records-query';
@@ -17,9 +18,14 @@ export const TableViewBody = () => {
       {records.map((record, index) => (
         <TableViewRow key={record.id} index={index} record={record} />
       ))}
-      {hasMore && (
-        <TableViewLoadMoreRow isPending={isPending} onClick={loadMore} />
-      )}
+      <InView
+        rootMargin="200px"
+        onChange={(inView) => {
+          if (inView && hasMore && !isPending) {
+            loadMore();
+          }
+        }}
+      ></InView>
     </div>
   );
 };

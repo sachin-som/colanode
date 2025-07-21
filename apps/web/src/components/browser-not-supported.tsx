24a91fc154272a6d8b8924149140ec035cbcab14
Hakan Shehu <hakanshehu15@gmail.com>
2025-07-21 10:02:02 +0200

    Improve browser not supported message (#142)

--- apps/web/src/components/browser-not-supported.tsx
+++ apps/web/src/components/browser-not-supported.tsx
@@ -7,22 +7,26 @@ export const BrowserNotSupported = () => {
         <MonitorOff className="h-10 w-10 text-gray-800" />
         <h2 className="text-4xl text-gray-800">Browser not supported</h2>
         <p className="text-sm text-gray-500">
-          Unfortunately, your browser does not support some features required
-          for Colanode to work. You can try using the{' '}
+          Unfortunately, your browser does not support the Origin Private File
+          System (OPFS) feature that Colanode requires to function properly.
+        </p>
+        <p className="text-sm text-gray-500">
+          If you're self-hosting Colanode make sure you are accessing the web
+          version through a secure 'https' way, because some browsers require
+          HTTPS to use the features required.
+        </p>
+        <p className="text-sm text-gray-500 mt-4">
+          You can try using the{' '}
           <a
             href="https://colanode.com/downloads"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-blue-500 font-semibold"
+            className="text-blue-500"
           >
             Desktop app
           </a>{' '}
-          instead or try another browser.
-        </p>
-        <p className="text-sm text-gray-500 mt-4">
-          Colanode requires Origin Private File System (OPFS) to work. If you
-          think this is a mistake or you have any questions, please open an
-          issue on{' '}
+          instead or try another browser. If you think this is a mistake or you
+          have any questions, please open an issue on{' '}
           <a
             href="https://github.com/colanode/colanode"
             target="_blank"
